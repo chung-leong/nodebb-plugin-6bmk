9bd12462ed11632f10c1aaa4f608183d9931e43a
Chung Leong <chernyshevsky@hotmail.com>
2023-03-26 17:31:48 +0200

    Checked in opening page prototype

---
+++ assets/typewriter.config.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  multipass: true, // boolean. false by default
+  js2svg: {
+    indent: 2, // string with spaces or number of spaces. 4 by default
+    pretty: true, // boolean, false by default
+  },
+  plugins: [
+    'removeDimensions',
+    {
+      name: 'preset-default',
+      params: {
+        overrides: {
+          cleanupIDs: {
+            preserve: [ 'roller', 'hammer', 'paper' ],
+            preservePrefixes: [ 'key-' ],
+          },
+          removeXMLProcInst: false,
+        },
+      },
+    },
+  ],
+};
